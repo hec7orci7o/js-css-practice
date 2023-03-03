8fb4bbef8e09c465bb83616067335ab450da7c6c
hector <hectortoraltrabajos@gmail.com>
2023-03-03 19:06:35 +0100

    animation

---
+++ data/images.js
@@ -0,0 +1,6 @@
+export const images = {
+  image1: 'https://tailwindui.com/img/ecommerce-images/product-page-05-related-product-01.jpg',
+  image2: 'https://tailwindui.com/img/ecommerce-images/product-page-05-related-product-02.jpg',
+  image3: 'https://tailwindui.com/img/ecommerce-images/product-page-05-related-product-03.jpg',
+  image4: 'https://tailwindui.com/img/ecommerce-images/product-page-05-related-product-04.jpg',
+};
